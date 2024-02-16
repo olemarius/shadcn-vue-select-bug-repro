79045b280bbc1afdab885502fb75d6ec01f4e4e9
olemarius <olemariusloset@gmail.com>
2024-02-16 09:25:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select';
+import { Card, CardContent } from './components/ui/card';
+</script>
+
+<template>
+  <div class="w-full p-10">
+    <Card>
+      <CardContent>
+        <Select data-cy="badge-requirement-timespan-select">
+          <SelectTrigger>
+            <SelectValue placeholder="Select option" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem v-for="option in 10" :key="option" :value="option">
+              Option {{ option }}
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardContent>
+    </Card>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
